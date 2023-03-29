bda3044371c61026ef47b1be3b4346fb8455207a
shawn <parvonisyego@gmail.com>
2023-03-29 07:26:36 +0300

    completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+    let form= document.querySelector('#create-task-form')
+  form.addEventListener('submit', (action)=> {
+    action.preventDefault()
+    displayTodo(form.querySelector("#new-task-description").value)
+    form.reset()
+  })
+
 });
+
+function displayTodo(todo){
+  let list= document.createElement('li')
+  list.textContent= `${todo} `
+  let btn= document.createElement('button')
+  btn.textContent= 'x'
+  btn.addEventListener('click', deleteTask)
+  list.appendChild(btn)
+  let tasks= document.querySelector('#tasks')
+  tasks.appendChild(list)
+}
+
+function deleteTask(action){
+  action.target.parentNode.remove()
+}
+  // your code here
+;
